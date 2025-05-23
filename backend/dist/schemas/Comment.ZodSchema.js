92c52784faf2b143e5160b5ec023b21cf4b80d33
Axel NGuessan <nguessanaxel21@gmail.com>
2025-05-23 15:38:28 +0000

    new world

--- backend/dist/schemas/Comment.ZodSchema.js
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-export const CommentZodValidator = z.object({
-    user: z.string().nonempty("User ID is required"),
-    post: z.string().nonempty("Post ID is required"),
-    content: z.string().nonempty("Comment content is required"),
-    createdAt: z.date().optional(),
-});
